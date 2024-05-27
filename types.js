456b12fc395fb397ec3ef7702f761a9d5ee3cf7c
Damien Seguin <dmnsgn@gmail.com>
2024-05-27 21:49:26 +0100

    build: update dev dependencies

--- types.js
+++ types.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * @typedef {Object} CanvasPatternOptions Options to draw. All optional.
+ * @typedef {object} CanvasPatternOptions Options to draw. All optional.
  * @property {CanvasPattern} [pattern] The CanvasPattern to draw. Created and returned by the function for reuse.
  * @property {CanvasPatternRepetition} [repetition] A DOMString indicating how to repeat the pattern's image.
  * @property {number} [x]
